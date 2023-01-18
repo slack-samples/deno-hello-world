3364f0535bf0580020bd272bbdbf383c55ac3306
Ashley <12901850+hello-ashleyintech@users.noreply.github.com>
2023-01-18 12:17:19 -0500

    Update capitalization for next-gen terms (#18)

--- triggers/greeting_trigger.ts
+++ triggers/greeting_trigger.ts
@@ -2,7 +2,7 @@ import { Trigger } from "deno-slack-api/types.ts";
 import GreetingWorkflow from "../workflows/greeting_workflow.ts";
 
 /**
- * Triggers determine when Workflows are executed. A trigger
+ * Triggers determine when workflows are executed. A trigger
  * file describes a scenario in which a workflow should be run,
  * such as a user pressing a button or when a specific event occurs.
  * https://api.slack.com/future/triggers
